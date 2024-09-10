ac754865136f771469f14ae8b3de8467e2d8eab3
Martin George <martingeorge191110@gmail.com>
2024-09-10 15:39:32 +0300

    Python - if/else, loops, functions! Task [101]

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+def remove_char_at(str, n):
+    if (len(str) - 1 < n or n < 0):
+        return str
+
+    newStr = ""
+    for i in str:
+        if i == str[n]:
+            continue
+
+        newStr += i
+
+    return newStr
