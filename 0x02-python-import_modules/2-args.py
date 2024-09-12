ad645d6a9adaeac09e2c7907961ec02e3c29e4d4
Martin George <martingeorge191110@gmail.com>
2024-09-12 13:10:03 +0300

    Python Modules Project: Task [2]

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,12 @@ if __name__ == "__main__":
     argvLen = len(argv)
     if argvLen == 1:
         print("0 arguments.")
+    elif argvLen == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(argvLen - 1))
-    i = 1
-    while i < argvLen:
-        print("{}: {}".format(i, argv[i]))
-        i += 1
+        i = 1
+        while i < argvLen:
+           print("{}: {}".format(i, argv[i]))
+           i += 1
