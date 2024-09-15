e7dfaf09dba44dd9b8fba0398968aff857c4559a
Martin George <martingeorge191110@gmail.com>
2024-09-15 15:14:42 +0300

    Python DS project 0x03: Taks [6]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for arr in matrix:
         for element in arr:
-            if element != arr[len(arr) - 1]:
+            if element != arr[-1]:
                 print("{:d}".format(element), end=" ")
             else:
                 print("{:d}".format(element))
