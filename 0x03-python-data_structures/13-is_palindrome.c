543ad1972d654cfd56dc1bee479c07779961cdfe
Martin George <martingeorge191110@gmail.com>
2024-09-16 13:02:26 +0300

    Python DS project 0x03: Taks [13]

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_palindrome - Function to Check whether the list is palindrome
+ * @head: pointer to the the head of the list
+ *
+ * Return: (0) Succes
+ * otherwiese - (1)
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp = *head;
+	int list[1024], index = 0, counter = 0;
+
+	if (!(*head))
+		return (NULL);
+
+	while (temp)
+	{
+		list[counter] = temp->n;
+		temp = temp->next;
+		counter++;
+	}
+
+	for (index = 0; index < counter; index++)
+	{
+		if (list[index] != list[counter - (index + 1)])
+			return (0);
+	}
+
+	return (1);
+}
