36be5444d1eb4973289948a511eb48f18c5df4bd
Martin George <martingeorge191110@gmail.com>
2024-09-18 18:02:29 +0300

    More Data Structure, Task [9]

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     newDic = {}
     for key in a_dictionary:
-        newDic[key] = newDic[key] * 2
+        newDic[key] = a_dictionary[key] * 2
     return (newDic)
