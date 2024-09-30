ed25858d503baf194950e09cef1f515d188da308
Martin George <martingeorge191110@gmail.com>
2024-09-30 17:56:36 +0300

    Python Exceptions, Task[2]

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    length = 0
+    for length in range(x):
+        try:
+            print("{:d}".format(my_list[length]), end='')
+            length += 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+    print()
+    return (length)
