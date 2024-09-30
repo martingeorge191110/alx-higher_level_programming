cce70e60f0d946881ff376c9a53dadef8e2e6abb
Martin George <martingeorge191110@gmail.com>
2024-09-30 17:57:58 +0300

    Python Exceptions, Task[2]

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,10 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[length]), end='')
             length += 1
         except TypeError:
+            length -= 1
             pass
         except ValueError:
+            length -= 1
             pass
     print()
     return (length)
