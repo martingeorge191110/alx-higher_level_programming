8eb890b01db111595685f73cfab60331a58de448
Martin George <martingeorge191110@gmail.com>
2024-09-30 17:47:39 +0300

    Python Exceptions, Task[0]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     length = 0
     for length in range(x):
         try:
-            print(f"{my_list[i]}", end='')
+            print(f"{my_list[length]}", end='')
             length += 1
         except IndexError:
             break
