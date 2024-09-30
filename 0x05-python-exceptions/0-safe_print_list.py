33a6f4ff101be5c1cdd1f958e5e23473a44caa93
Martin George <martingeorge191110@gmail.com>
2024-09-30 17:44:25 +0300

    Python Exceptions, Task[0]

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    length = 0
+    for length in range(x):
+        try:
+            print(f"{my_list[i]}", end='')
+            length += 1
+        except IndexError:
+            break
+    print('\n')
+    return (length)
