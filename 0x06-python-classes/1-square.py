25f0559e2d11b3b4cbc027ffb1df5ecc68717649
Martin George <martingeorge191110@gmail.com>
2024-10-01 17:03:01 +0300

    Classes Project, Task[1]

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Clas with Attributes"""
+
+
+class Square:
+    """Square Class with size Attribute"""
+    def __init__(self, size) -> None:
+        """Attributes initialize"""
+        self.__size = size
