15881541d353da4b03d907be252ee2f4ec77573c
Martin George <martingeorge191110@gmail.com>
2024-10-01 17:12:55 +0300

    Classes Project, Task[2]

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Square Class with size Attribute"""
     def __init__(self, size=0):
-        """Initialize an Instance"""
+        """Initialize an Instance with size Attribute"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
