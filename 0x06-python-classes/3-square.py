61cc8e1dea52fd13c3ea074d4d87b3367c65aaf0
Martin George <martingeorge191110@gmail.com>
2024-10-01 17:18:11 +0300

    Classes Project, Task[3]

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
-"""Class with Attributes"""
+"""Class With Attribute"""
 
 
-class 
+class Square:
+    """Square Class with size Attribute"""
+    def __init__(self, size=0):
+        """Initialize an Instance with size Attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Function to Calc Area
+        """
+        return (self.__size * self.__size)
