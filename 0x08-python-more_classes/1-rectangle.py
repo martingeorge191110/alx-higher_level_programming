d3cdd96e614a79f5c8ff6b5132ffc2a656936c93
Martin George <martingeorge191110@gmail.com>
2024-10-07 17:17:19 +0300

    More classes Project, Task[1]

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,13 +14,13 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """Function to retrieve width"""
-        return (self.width)
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -29,7 +29,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
