f1c70115687fec246f24fb05c20e16325c72fa6c
Martin George <martingeorge191110@gmail.com>
2024-10-07 17:16:15 +0300

    More classes Project, Task[1]

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+
+
+class Rectangle:
+    """Rectangle Class"""
+
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Function to retrieve width"""
+        return (self.width)
+
+    @width.setter
+    def width(self, value):
+        """Function to set width value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
+    @property
+    def height(self):
+        """Function to retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """function to set new height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
