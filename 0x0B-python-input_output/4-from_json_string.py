9e7bc5f18e494a391573682791802c489c132937
Martin George <martingeorge191110@gmail.com>
2024-10-15 18:02:35 +0300

    File input and output: Task [4]

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""from json to string module"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Function to returns an object in json string"""
+    return (json.loads(my_str))
