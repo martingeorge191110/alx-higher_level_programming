31ee1caba8bfe4461abc1ea464bdaf70d23dc9e3
Martin George <martingeorge191110@gmail.com>
2024-10-15 18:00:38 +0300

    File input and output: Task [3]

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""json string module"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """function to returns the JSON representation"""
+    return (json.dumps(my_obj))
