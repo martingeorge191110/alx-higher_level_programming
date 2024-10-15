6464d903c204fb387a9cb8b5bd5bddb5eba75e4c
Martin George <martingeorge191110@gmail.com>
2024-10-15 17:12:47 +0300

    File input and output: Task [0]

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 
 def read_file(filename=""):
     """Function to read file and print"""
-    with open(filename, encoding="utf-8") as file
-    for line in file:
-        print(line, end="")
+    with open(filename, encoding="utf-8") as file:
+       for line in file:
+          print(line, end="")
