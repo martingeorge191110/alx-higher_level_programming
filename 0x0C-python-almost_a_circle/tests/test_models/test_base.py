b9b11967b75892a5da1443440d6e5cb9e63dbb9d
Martin George <martingeorge191110@gmail.com>
2024-10-21 15:00:48 +0300

    Base module test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
-"""Test Base class module"""
-import unittest
-from models import base
+"""test module Base class"""
 
+import unittest
+from models.base import Base
 
 class BaseTest(unittest.TestCase):
     """Test BaseClass"""
 
-    def test_setId(self):
-        result = base.Base(20)
-        result2 = base.Base(None)
-        self.assertEqual(result.id, 20)
-        self.assertEqual(result2.id, 1)
+    def setUp(self):
+        # Accessing the private attribute directly to reset it
+        Base._Base__nb_objects = 0
+
+    def test_input_id(self):
+        result = Base(12)
+        self.assertEqual(result.id, 12)
+
+    def test_nb_objects_id(self):
+        res1 = Base()
+        res2 = Base()
+        res3 = Base()
+        self.assertEqual(res1.id, 1)
+        self.assertEqual(res2.id, 2)
+        self.assertEqual(res3.id, 3)
+
+    def test_combined_id(self):
+        res1 = Base()
+        res2 = Base(30)
+        res3 = Base()
+        self.assertEqual(res1.id, 1)
+        self.assertEqual(res2.id, 30)
+        self.assertEqual(res3.id, 2)
+
+    def test_two_args(self):
+        with self.assertRaises(TypeError):
+            result = Base(1, 2)
 
 if __name__ == "__main__":
     unittest.main()
