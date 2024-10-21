d9fb49a0db98356fcf89def3585aaee0427dcda5
Martin George <martingeorge191110@gmail.com>
2024-10-21 13:59:02 +0300

    TASK (11)

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,14 @@ class Square(Rectangle):
 
     def __str__(self):
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    @property
+    def size(self):
+        """get square size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set new size"""
+        self.width = value
+        self.height = value
