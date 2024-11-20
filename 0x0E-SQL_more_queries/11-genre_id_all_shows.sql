2d02cf6cb638ae1d7cbdcf68222a41c3ee0939ff
Martin George <martingeorge191110@gmail.com>
2024-11-20 18:08:03 +0200

    Task 12

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 --  lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+SELECT ts.title, tg.genre_id
+FROM tv_shows t
+LEFT JOIN tv_show_genres tg
+ON ts.id = tg.show_id
+ORDER BY ts.title, tg.genre_id
