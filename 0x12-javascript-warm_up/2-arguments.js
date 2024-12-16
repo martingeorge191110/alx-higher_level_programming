71836058b73638fa22f5b59dcfb242ea65c0f427
Martin George <martingeorge191110@gmail.com>
2024-12-16 14:46:08 +0200

    Task 2 file style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-const prc = process.argv
+const prc = process.argv;
 
-if (prc.length === 2)
-   console.log("No argument")
-else if (prc.length === 3)
-   console.log("Argument found")
-else
-   console.log("Arguments found")
+if (prc.length === 2) {
+  console.log('No argument');
+} else if (prc.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
