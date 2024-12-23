71c809ed3e3ef88b96e3a45526bb91d44cb64bee
Martin George <martingeorge191110@gmail.com>
2024-12-23 17:30:37 +0200

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT name FROM customer WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+
+    states = cur.fetchall()
+
+    for i in range(len(states)):
+        print(f"{states[i]}")
+
+    cur.close()
+    db.close()
