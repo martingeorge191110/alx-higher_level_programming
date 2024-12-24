8d1a179357816e83359254b5a7a80e3e22349bb4
Martin George <martingeorge191110@gmail.com>
2024-12-24 15:20:20 +0200

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+
+if __name__ == "__main__":
+    connection = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    eng = create_engine(connection)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State)
+
+    for i in result:
+        print(f"{i.id}: {i.name}")
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """python file that contains the class
-   definition of a State and
-   an instance Base = declarative_base()
-   using SQLAlchemy"""
+    definition of a State and
+    an instance Base = declarative_base()
+    using SQLAlchemy"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
-class State(Base):
-   """class state that defines a state"""
 
-   __tablename__ = "states"
-   id = Column(Integer, nullable=False, unique=True, primary_key=True)
-   name = Column(String(128), nullable=False)
+class State(Base):
+    """class state that defines a state"""
+
+    __tablename__ = "states"
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
