3365fbb6d4204405be1c8c359e2ce0df54c6d35d
Martin George <martingeorge191110@gmail.com>
2025-01-16 16:24:19 +0200

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers"""
+    myList = list_of_integers
+
+    if myList == []:
+        return (None)
+
+    temp = myList[0]
+    for i in range(0, len(myList) - 1):  # Prevent IndexError
+        if myList[i] >= myList[i + 1]:
+            temp = myList[i + 1]
+            myList[i + 1] = myList[i]
+            myList[i] = temp
+
+    return myList[-1]
+
