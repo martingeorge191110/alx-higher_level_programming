c140953ae2284a9e2fbc048edac3a7e88c915dfe
Martin George <martingeorge191110@gmail.com>
2025-01-21 16:10:06 +0200

    Task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python script that list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[1], sys.argv[2]
+        )
+
+        response = requests.get(url)
+
+        for i in range(10):
+            commit = response.json()[i]
+
+            print("{}: {}".format(
+                commit.get("sha"),
+                commit.get("commit").get("author").get("name")
+            ))
+    except Exception:
+        pass
